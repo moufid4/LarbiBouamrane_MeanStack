7b37464989dbd2a566cf3e3392dae8524e38b5e5
Moufid Larbi <moufid4@gmail.com>
2015-05-23 19:46:26 +0100

    changing home to accueil

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -3,7 +3,7 @@
 angular.module('larbiAngularApp')
   .controller('NavbarCtrl', function ($scope, $location, Auth) {
     $scope.menu = [{
-      'title': 'Home',
+      'title': 'Accueil',
       'link': '/'
     },
     {
